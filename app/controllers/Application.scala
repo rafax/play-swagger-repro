84ac5059f294ab56db6e3d0ccd924e6e21efe6d5
Rafal Gajdulewicz <rafal.gajdulewicz@vimn.com>
2016-01-04 12:39:08 +0100

    Fix Readme rendering on github

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,9 @@
 package controllers
 
-import io.swagger.annotations.Api
 import models.BirthDate
 import org.joda.time.DateTime
 import play.api.mvc._
 
-@Api
 class Application extends Controller {
   def age(birthDate: BirthDate) = Action {
     Ok(DateTime.now().minusYears(birthDate.date.year().get()).getYear.toString)
